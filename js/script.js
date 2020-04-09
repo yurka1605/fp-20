68235f2582860c9a83f0cf36b6d8ce8a683d4c50
Litvinov Yuriy <u.litvinov@elewise.com>
2020-04-09 11:22:42 +0400

    feat(block): add bottom banner

--- js/script.js
+++ js/script.js
@@ -1,25 +1,57 @@
 $( document ).ready(function() {
-    
-});
+    $('#topPhone').mask('+7 (999) 999-9999');
+    $('#bottomPhone').mask('+7 (999) 999-9999');
 
-$(window).on('scroll', function() {
-    // $(window).scrollTop() > $('.main').offset().top ? 
-    //     $('.fixed-form').addClass('show') : $('.fixed-form').removeClass('show');
+    var timeID = setInterval(() => {
+        const hoursHtml = $('.time .hours');
+        const minutesHtml = $('.time .min');
+        const secHtml = $('.time .sec');
+        const hours = parseInt($(hoursHtml).html(), 10);
+        const min = parseInt($(minutesHtml).html(), 10);
+        const sec = parseInt($(secHtml).html(), 10);
+
+        if (hours === 0 && min === 0 && sec === 0) {
+            clearInterval(timeID);
+        } else {
+            if (min === 0 && sec === 0) {
+                $(hoursHtml).text(hours > 10 ? `${ hours - 1 }` : `0${ hours - 1 }`);
+                $(minutesHtml).text('59');
+                $(secHtml).text('59');
+            } else {
+                if (sec === 0) {
+                    $(minutesHtml).text(min > 10 ? `${ min - 1 }` : `0${ min - 1 }`);
+                    $(secHtml).text('59');
+                } else {
+                    $(secHtml).text(sec > 10 ? `${ sec - 1 }` : `0${ sec - 1 }`);
+                }
+            }
+        }
+    }, 1000);
 });
 
 /** ======================== User actions ========================== **/
-// scrool top
-$('.footer .further__arrow').on('click', function(e) {
-    e.preventDefault();
-    $('body, html').animate({ 
-        scrollTop: 0,
-    }, 300, 'swing');
-})
-
+// slider arrows
 $('.slider__control').on('click', function(e) {
     e.preventDefault();
     const slider = $(this).parent().parent();
     changeSlide(slider, $(this).hasClass('left'));
+});
+
+// send form 
+$('.banner__form button').click(function(e) {
+    e.preventDefault();
+    var data = {
+        "Имя": $(this).parent().children('.form__inputs').children('input[name="name"]').val(),
+        "Телефон": $(this).parent().children('.form__inputs').children('input[name="phone"]').val(),
+    }
+    $.ajax({
+        type: 'POST',
+        url: 'https://yurka1605.github.io/uropoten/mail.php',
+        data: data
+    }).done(() => {
+        $(this).parent().children('.form__inputs').children('input[name="name"]').val('');
+        $(this).parent().children('.form__inputs').children('input[name="phone"]').val('');
+    });
 });
 /** ======================== END:User actions ========================== **/
 
